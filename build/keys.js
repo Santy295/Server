774f10168c97609e674f58cf8b92a9c90b226470
Santy295 <santynavarro29@hotmail.com>
2020-08-05 13:20:57 -0500

    Base de datos remota keys.js

--- build/keys.js
+++ build/keys.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: 'santy295',
-        database: 'tmy_quito'
+        host: 'remotemysql.com',
+        user: '1UizlKY2cn',
+        password: 'XF4AQdbrqq',
+        database: '1UizlKY2cn'
     }
 };
